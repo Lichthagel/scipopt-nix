938a297fa23f64bed838730191fdf2312badb6f3
Jens Gatzweiler <jens.gatzweiler@rwth-aachen.de>
2024-02-12 22:41:18 +0100

    use scipoptsuite-src for gcg

--- gcg.nix
+++ gcg.nix
@@ -1,17 +1,14 @@
 {
   pkgs ? import <nixpkgs> {},
   scip ? pkgs.callPackage ./scip.nix {},
+  scipoptsuite-src ? pkgs.callPackage ./scipoptsuite-src.nix {},
   ...
 }:
-pkgs.stdenv.mkDerivation rec {
+pkgs.stdenv.mkDerivation {
   pname = "gcg";
   version = "3.5.5";
 
-  src = builtins.fetchGit {
-    url = "git@git.or.rwth-aachen.de:gcg/gcg.git";
-    ref = "v${builtins.replaceStrings ["."] [""] version}";
-    rev = "da78996a24894c343b21d3abbefe5f1251156b39";
-  };
+  src = assert scipoptsuite-src.version == "8.1.0"; "${scipoptsuite-src}/gcg"; # scipoptsuite 8.1.0 ships gcg 3.5.5
 
   nativeBuildInputs = with pkgs; [
     cmake
@@ -32,7 +29,7 @@ pkgs.stdenv.mkDerivation rec {
     "-DSYM=none"
   ];
 
-  preConfigure = ''
-    echo "#define GCG_GITHASH \"${src.shortRev}\"" > ./src/githash.c
-  '';
+  # preConfigure = ''
+  #   echo "#define GCG_GITHASH \"${src.shortRev}\"" > ./src/githash.c
+  # '';
 }
