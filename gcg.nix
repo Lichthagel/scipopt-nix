47783e7c4e7e7776ea92d5af1f81e965ead2d835
Jens Gatzweiler <jens.gatzweiler@rwth-aachen.de>
2024-02-12 17:40:48 +0100

    Update gcg to version 3.5.5

--- gcg.nix
+++ gcg.nix
@@ -5,12 +5,12 @@
 }:
 pkgs.stdenv.mkDerivation rec {
   pname = "gcg";
-  version = "3.5.4";
+  version = "3.5.5";
 
   src = builtins.fetchGit {
     url = "git@git.or.rwth-aachen.de:gcg/gcg.git";
     ref = "v${builtins.replaceStrings ["."] [""] version}";
-    rev = "5f944b20d4ac6eaca27445dacd0e188db3294d60";
+    rev = "da78996a24894c343b21d3abbefe5f1251156b39";
   };
 
   nativeBuildInputs = with pkgs; [
