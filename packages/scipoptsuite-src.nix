b02f8687ca85b2697c3b61f130c9164252f39508
Jens Gatzweiler <jens.gatzweiler@rwth-aachen.de>
2024-06-22 10:18:42 +0200

    suite-src: update 9.1.0

--- packages/scipoptsuite-src.nix
+++ packages/scipoptsuite-src.nix
@@ -1,10 +1,10 @@
 {pkgs ? import <nixpkgs> {}, ...}:
 pkgs.fetchzip rec {
   pname = "scipoptsuite-src";
-  version = "9.0.1";
+  version = "9.1.0";
 
   name = "${pname}-${version}";
 
   url = "https://scipopt.org/download/release/scipoptsuite-${version}.tgz";
-  hash = "sha256-dc2NczLSgc7f48QCqZfRC4nOGov57B5ovoqSMIt51NI=";
+  hash = "sha256-30rxnlP1DuMo0/F3mDDwpGeTKCzf7B0nsUsfSY/lB54=";
 }
